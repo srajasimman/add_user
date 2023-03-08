33ed7197cf00288096a41f625d65d2526f883c19
Rajasimman S <srajasimman@gmail.com>
2023-03-08 22:19:05 +0530

    :tada: Initial commit

---
+++ add_user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+user_name="$1"
+ssh_key="$2"
+password="$3"
+
+if [[ -z "$user_name" ]] || [[ -z "$ssh_key" ]]; then
+    echo "Usage: $0 <user_name> <ssh_key> <password>"
+    exit 1
+fi
+
+if [[ -z "$password" ]]; then
+    password=$(date +%s | sha256sum | base64 | head -c 32)
+fi
+
+if [[ ! -f "./add_user.yml" ]]; then
+    wget -O ./add_user.yml https://raw.githubusercontent.com/srajasimman/add_user/main/add_user.yml || exit 1
+fi
+
+ansible-playbook add_user.yml --extra-vars "user_name=$user_name ssh_key='$ssh_key' password=$password"
